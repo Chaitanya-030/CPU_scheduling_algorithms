4b3c5e4bc50ef919fb293209b994cd20f08436c9
chaitanya <csheth845@gmail.com>
2024-06-10 09:49:58 +0530

    priority preemptive added

--- priority_p.cpp
+++ priority_p.cpp
@@ -5,6 +5,7 @@ struct process {
     int pid; // process id
     int at; // arrival time
     int bt; // burst time
+    int priority; // priority
     int ct, wt, tat, rt, start_time; // completion time, wait time, turn around time, response time
 }ps[100]; // array of struct process for 'n' processes
 
@@ -15,7 +16,7 @@ int main()
     cin>>n;
     bool is_completed[100], first_process = true;
     int curr_time = 0, completed = 0;
-    float bt_remaining[100]; // this will contain the values when the process will preempt
+    int bt_remaining[100]; // this will contain the values when the process will preempt
     
     int sum_tat=0, sum_wt=0, sum_rt=0, idle_time=0, prev=0, cycle_length;
     float cpu_utilization;
@@ -36,53 +37,53 @@ int main()
         bt_remaining[i] = ps[i].bt;
     }
     
+    // Taking the priority input
+    for(int i=0;i<n;i++) {
+        cout<<"Enter process "<<i<<" priority: ";
+        cin>>ps[i].priority;
+    }
+    
     while(completed < n) {
-        //find the process with min. burst time in ready queue at current time (at <= curr_time)
-        int min_ind = -1;
-        int mini = INT_MAX;
+        //find the process with max. priority in ready queue at current time (at <= curr_time)
+        int max_ind = -1;
+        int maxi = INT_MIN;
         
         for(int i=0;i<n;i++) {
             if(ps[i].at <= curr_time && !is_completed[i]) {
-                if(bt_remaining[i] < mini) {
-                    mini = bt_remaining[i];
-                    min_ind = i;
+                if(ps[i].priority > maxi) {
+                    maxi = ps[i].priority;
+                    max_ind = i;
                 }
-                if(bt_remaining[i] == mini) {
-                    if(ps[i].at < ps[min_ind].at) {
-                        mini = bt_remaining[i];
-                        min_ind = i;
+                if(ps[i].priority == maxi) {
+                    if(ps[i].at < ps[max_ind].at) {
+                        maxi = ps[i].priority;
+                        max_ind = i;
                     }
                 }
             }
         }
         
-        if(min_ind == -1) curr_time++;
+        if(max_ind == -1) curr_time++;
         else {
-            if(bt_remaining[min_ind] == ps[min_ind].bt) {
-                ps[min_ind].start_time = curr_time;
-                if(!first_process) {
-                    idle_time += ps[min_ind].start_time - prev;
-                }
-                first_process = false;
+            if(bt_remaining[max_ind] == ps[max_ind].bt) {
+                ps[max_ind].start_time = curr_time;
             }
-            
-            bt_remaining[min_ind]--; // as we are checking every one second, we will perform this operation
-            // by 1 second and then again go into the while loop and check for the min burst time 
+            bt_remaining[max_ind]--;
             curr_time++;
-            prev = curr_time;
             
-            if(bt_remaining[min_ind] == 0) {
-                ps[min_ind].ct = curr_time;
-                ps[min_ind].tat = ps[min_ind].ct - ps[min_ind].at;
-                ps[min_ind].wt= ps[min_ind].tat - ps[min_ind].bt;
-                ps[min_ind].rt = ps[min_ind].start_time - ps[min_ind].at;
-            
-            
-                sum_tat += ps[min_ind].tat;
-                sum_wt += ps[min_ind].wt;
-                sum_rt += ps[min_ind].rt;
+            if(bt_remaining[max_ind] == 0) {
+                // ps[max_ind].start_time = curr_time;
+                ps[max_ind].ct = curr_time;
+                ps[max_ind].tat = ps[max_ind].ct - ps[max_ind].at;
+                ps[max_ind].wt = ps[max_ind].tat - ps[max_ind].bt;
+                ps[max_ind].rt = ps[max_ind].start_time - ps[max_ind].at;
+                
+                
+                sum_tat += ps[max_ind].tat;
+                sum_wt += ps[max_ind].wt;
+                sum_rt += ps[max_ind].rt;
                 completed++;
-                is_completed[min_ind] = true;
+                is_completed[max_ind] = true;
             }
         }
     }
@@ -102,13 +103,10 @@ int main()
      cout<<endl;  
     }
     
-    cpu_utilization = (float)(cycle_length - idle_time)/cycle_length;
-
     cout<<"Average Turn Around time= "<< (float)sum_tat/n<<endl;
     cout<<"Average Waiting Time= "<<(float)sum_wt/n<<endl;
     cout<<"Average Response Time= "<<(float)sum_rt/n<<endl;    
     cout<<"Throughput= "<<n/(float)cycle_length<<endl;
-    cout<<"CPU Utilization(Percentage)= "<<cpu_utilization*100;
 
     return 0;
 }
